197976fdffb90f18549f4f9cf2a04c1b97375b61
Daniel Szucs <daniel.git@pm.me>
2023-12-27 20:31:18 +0100

    Add proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 fn main() {
-    let prufer = [1, 1, 1, 1, 6, 5];
-    let edges = tree_edges(&prufer);
-    println!("The edge set E(G) is:\n{:?}", &edges);
+    let prufer = [1, 1, 1, 1, 9, 5];
+    let res = tree_edges(&prufer);
+    match res {
+        Ok(edges) => println!("The edge set E(G) is:\n{:#?}", &edges),
+        Err(e) => println!("{}", e.to_string()),
+    }
 }
 
-fn tree_edges(prufer_code: &[usize]) -> Vec<(usize, usize)> {
+fn tree_edges(prufer_code: &[usize]) -> Result<Vec<(usize, usize)>, PruferError> {
     let vertecies = prufer_code.len() + 2;
     let mut vertex_set: Vec<i64> = [0].repeat(vertecies);
 
     for &code in prufer_code.iter() {
         if code > vertecies {
-            panic!(r#"Invalid code in sequence: "{code}""#);
+            return Err(PruferError::InvalidCode {
+                code,
+                seq: prufer_code,
+            });
         }
 
         vertex_set[code - 1] += 1;
     }
 
-    let mut edges: Vec<(usize, usize)> = Vec::with_capacity(vertecies - 1);
+    let mut edges: Vec<(usize, usize)> = Vec::with_capacity(vertecies + 1);
 
     for &code in prufer_code.iter() {
         for (j, v) in vertex_set.iter_mut().enumerate() {
@@ -40,32 +46,83 @@ fn tree_edges(prufer_code: &[usize]) -> Vec<(usize, usize)> {
     }
 
     edges.push(create_last_pair(&vertex_set));
-    edges
+    Ok(edges)
+}
+
+#[derive(Debug, PartialEq)]
+enum PruferError<'a> {
+    InvalidCode { code: usize, seq: &'a [usize] },
+}
+
+impl<'a> ToString for PruferError<'a> {
+    fn to_string(&self) -> String {
+        match self {
+            PruferError::InvalidCode { code, seq } => {
+                format!(
+                    r#"Invalid code in sequence:
+    P = {:?}
+    N = |P| + 2 = {}
+    max(P) = {}
+    max(P) > N => Invalid prüfer sequence"#,
+                    seq,
+                    seq.len() + 2,
+                    code
+                )
+            }
+        }
+    }
 }
 
 #[test]
-fn test_1() {
-    const PRUFER_CODE: [usize; 5] = [5, 1, 2, 4, 3];
-    let edges = tree_edges(&PRUFER_CODE);
-    assert_eq!(
-        [(6, 5), (5, 1), (1, 2), (2, 4), (4, 3), (3, 7)],
-        edges.as_slice()
-    );
-}
-
-#[test]
-fn test_2() {
+fn test_prufer_4() {
     const PRUFER_CODE: [usize; 4] = [4, 1, 3, 4];
-    let edges = tree_edges(&PRUFER_CODE);
-    assert_eq!([(2, 4), (5, 1), (1, 3), (3, 4), (4, 6)], edges.as_slice());
+    let res = tree_edges(&PRUFER_CODE);
+
+    match res {
+        Ok(edges) => assert_eq!([(2, 4), (5, 1), (1, 3), (3, 4), (4, 6)], edges.as_slice()),
+        Err(e) => assert!(false, "{}", e.to_string()),
+    }
 }
 
 #[test]
-fn test_3() {
+fn test_prufer_5() {
+    const PRUFER_CODE: [usize; 5] = [5, 1, 2, 4, 3];
+    let res = tree_edges(&PRUFER_CODE);
+
+    match res {
+        Ok(edges) => assert_eq!(
+            [(6, 5), (5, 1), (1, 2), (2, 4), (4, 3), (3, 7)],
+            edges.as_slice()
+        ),
+        Err(e) => assert!(false, "{}", e.to_string()),
+    }
+}
+
+#[test]
+fn test_prufer_6() {
     const PRUFER_CODE: [usize; 6] = [1, 1, 1, 1, 6, 5];
-    let edges = tree_edges(&PRUFER_CODE);
+    let res = tree_edges(&PRUFER_CODE);
+
+    match res {
+        Ok(edges) => assert_eq!(
+            [(2, 1), (3, 1), (4, 1), (7, 1), (1, 6), (6, 5), (5, 8)],
+            edges.as_slice()
+        ),
+        Err(e) => assert!(false, "{}", e.to_string()),
+    }
+}
+
+#[test]
+fn test_invalid_code() {
+    const WRONG_CODE: usize = 7;
+    const PRUFER_CODE: [usize; 4] = [4, WRONG_CODE, 3, 4];
+    let res = tree_edges(&PRUFER_CODE);
+
     assert_eq!(
-        [(2, 1), (3, 1), (4, 1), (7, 1), (1, 6), (6, 5), (5, 8)],
-        edges.as_slice()
+        res,
+        Err(PruferError::InvalidCode {
+            code: WRONG_CODE,
+            seq: &PRUFER_CODE
+        })
     );
 }
