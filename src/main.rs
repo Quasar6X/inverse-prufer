cea23d6df301a2c528b3a9ad5403a3aa8e1878c8
Daniel Szucs <daniel.git@pm.me>
2023-12-30 11:19:04 +0100

    Implement argument parsing
    The prüfer code should be given through the command line.
    Codes are passed one by one delimited by spaces.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use clap::Parser;
 use inverse_prufer::tree_edges;
+
+#[derive(Parser, Debug)]
+#[command(about, author, version)]
+struct Cli {
+    /// Prüfer sequence
+    #[arg(name = "SEQ", required = true, value_delimiter = ' ', value_parser = clap::value_parser!(u64).range(1..))]
+    code: Vec<u64>,
+}
 
 #[doc(hidden)]
 fn main() {
-    let prufer = [4, 7, 3, 4];
-    let res = tree_edges(&prufer);
+    let args = Cli::parse();
+    let res = tree_edges(&args.code);
     match res {
-        Ok(edges) => println!("The edge set E(G) is:\n{:#?}", &edges),
-        Err(e) => println!("{}", e.to_string()),
+        Ok(edges) => println!("The edge set E(G) is:\n{:?}", &edges),
+        Err(e) => eprintln!("{}", e.to_string()),
     }
 }
 
